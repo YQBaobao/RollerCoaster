914d26b30480d352c665098d22de300ce67d8feb
yqbaowo <yqbaowo@qq.com>
2024-10-12 16:45:10 +0800

    FIX:更新检查方案

--- core/rc.py
+++ core/rc.py
@@ -69,8 +69,6 @@ class RollerCoasterApp(QWidget, Ui_RollerCoaster):
         self.polling_status = 1
         self.background_button = True  # 背景色按钮状态
         self.msg_status = True  # 消息提醒状态
-        self.icon_count = 0  # 默认图标数量
-        self.icon_status = True
         self.check_update_status = False  # 未检查更新的标志
 
         self.base_signal = BaseSignal()
@@ -109,47 +107,34 @@ class RollerCoasterApp(QWidget, Ui_RollerCoaster):
     def set_taskbar(self):
         """设置任务栏"""
         self.m_h_taskbar = win32gui.FindWindow("Shell_TrayWnd", None)  # 任务栏“Shell_TaryWnd”的窗口句柄
-        self.m_h_bar = m_h_bar = win32gui.FindWindowEx(self.m_h_taskbar, 0, "ReBarWindow32",
-                                                       None)  # 子窗口“ReBarWindow32”的窗口句柄
-        self.m_h_min = m_h_min = win32gui.FindWindowEx(m_h_bar, 0, "MSTaskSwWClass", None)  # 子窗口“MSTaskSwWClass”的窗口句柄
-        self.b = b = win32gui.GetWindowRect(m_h_bar)  # 获取m_hBar窗口尺寸b为[左，上，右，下]的数组
-        win32gui.MoveWindow(m_h_min, 0, 0, b[2] - b[0] - 75, b[3] - b[1], True)  # 调整m_hMin的窗口大小，为我们的程序预留出位置
+        self.m_h_bar = win32gui.FindWindowEx(self.m_h_taskbar, 0, "ReBarWindow32",
+                                             None)  # 子窗口“ReBarWindow32”的窗口句柄
+        self.m_h_min = win32gui.FindWindowEx(self.m_h_bar, 0, "MSTaskSwWClass", None)  # 子窗口“MSTaskSwWClass”的窗口句柄
+        self.b = win32gui.GetWindowRect(self.m_h_bar)  # 获取m_hBar窗口尺寸b为[左，上，右，下]的数组
 
-        self.setGeometry(b[2] - b[0] - 75, -5, 75, b[3] - b[1])  # 调整我们自己的窗口到预留位置的大小
-        win32gui.SetParent(int(self.winId()), m_h_bar)  # 将我们自己的窗口设置为m_hBar的子窗口
+        self.move_window()
 
-    def timer_set_taskbar(self, interval: int = 500):  # 500ms
+    def move_window(self):
+        # 调整m_hMin的窗口大小，为我们的程序预留出位置
+        win32gui.MoveWindow(self.m_h_min, 0, 0, self.b[2] - self.b[0] - 75, self.b[3] - self.b[1], True)
+
+        self.setGeometry(self.b[2] - self.b[0] - 75, -5, 75, self.b[3] - self.b[1])  # 调整我们自己的窗口到预留位置的大小
+        win32gui.SetParent(int(self.winId()), self.m_h_bar)  # 将我们自己的窗口设置为m_hBar的子窗口
+
+    def timer_set_taskbar(self, interval: int = 200):  # 500ms
         """定时设置任务栏"""
         self.time_set_taskbar = QTimer(self)
         self.time_set_taskbar.setInterval(interval)
-        self.time_set_taskbar.timeout.connect(self.get_tray_icon_count)
+        self.time_set_taskbar.timeout.connect(self.get_taskbar_size)
         self.time_set_taskbar.start()  # 启动
 
-    def get_tray_icon_count(self) -> int:
-        # 获取托盘区域的窗口句柄
-        tray_notify_handle = win32gui.FindWindowEx(self.m_h_taskbar, 0, "TrayNotifyWnd", None)
-        sys_pager_handle = win32gui.FindWindowEx(tray_notify_handle, 0, "SysPager", None)
-        notification_area_handle = win32gui.FindWindowEx(sys_pager_handle, 0, "ToolbarWindow32", None)
-        # 获取托盘图标的数量
-        count = win32gui.SendMessage(notification_area_handle, commctrl.TB_BUTTONCOUNT, 0, 0)
-        if self.icon_status:
-            self.icon_count = count  # 初始化
-            self.icon_status = False
-        if self.icon_count != count:
-            self.dynamic_set_taskbar(icon_count=count)
-        else:
-            return 0
-
-    def dynamic_set_taskbar(self, icon_count=0):
-        """动态设置任务栏"""
-        if icon_count and self.icon_count != 0:
-            x = self.icon_count - icon_count
-            self.b = (self.b[0], self.b[1], self.b[2] + (x * 24), self.b[3],)
-            self.icon_count = icon_count
-        win32gui.MoveWindow(self.m_h_min, 0, 0, self.b[2] - self.b[0] - 75, self.b[3] - self.b[1], True)
-
-        self.setGeometry(self.b[2] - self.b[0] - 75, -5, 75, self.b[3] - self.b[1])
-        win32gui.SetParent(int(self.winId()), self.m_h_bar)
+    def get_taskbar_size(self):
+        """获取任务栏尺寸"""
+        self.b_new = win32gui.GetWindowRect(self.m_h_bar)
+        if self.b_new == self.b:  # 尺寸没变化，则直接返回
+            return
+        self.b = self.b_new
+        self.move_window()
 
     def init_action(self):
         """信号"""
