dacbcfb6dea5321f69c139b0f828c6d1446f60c2
yqbaowo <yqbaowo@qq.com>
2024-12-30 15:55:12 +0800

    UPDATE:version

--- core/__init__.py
+++ core/__init__.py
@@ -7,6 +7,6 @@
 @ Version     : V1.0.0
 @ Description : 
 """
-version = '1.1.0.0'
+version = '1.1.0.0_beta'
 gitee_url = 'https://gitee.com/api/v5/repos/yqbao/roller-coaster/tags?sort=name&direction=desc&page=1&per_page=5'
 github_url = 'https://api.github.com/repos/YQBaobao/RollerCoaster/tags'
