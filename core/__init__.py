df82bb2980ce26eabb05970b0d202657a74bf362
yqbaowo <yqbaowo@qq.com>
2025-05-09 16:36:57 +0800

    2.0.1.0

--- core/__init__.py
+++ core/__init__.py
@@ -8,7 +8,7 @@
 @ Description : 
 """
 APP_NAME = "RollerCoaster"
-VERSION = '2.0.0.3'
+VERSION = '2.0.1.0'
 
 gitee_url = 'https://gitee.com/api/v5/repos/yqbao/roller-coaster/tags?sort=name&direction=desc&page=1&per_page=5'
 github_url = 'https://api.github.com/repos/YQBaobao/RollerCoaster/tags'
